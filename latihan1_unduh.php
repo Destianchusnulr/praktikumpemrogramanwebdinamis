c7a736f6497b607d75bb190fb6ce342408327074
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-10-08 05:29:56 +0700

    Add files via upload

---
+++ latihan1_unduh.php
@@ -0,0 +1,8 @@
+<?php
+$myDir = "c:/wamp/www/1900018038/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
