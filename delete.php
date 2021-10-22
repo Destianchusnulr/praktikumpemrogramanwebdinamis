a47d7e7fdf202e551d7d162ad8eb488577a0ffdf
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-10-23 06:46:29 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,10 +1,6 @@
 <?php
-// include database connection file
-include_once("koneksi.php");
-// Get id from URL to delete that user
-$nim = $_GET['nim'];
-// Delete user row from table based on given id
-$result = mysqli_query($con, "DELETE FROM mahasiswa WHERE nim=’$nim’");
-// After delete redirect to Home, so that latest user list will be displayed.
-header("Location:index.php");
+include_once("koneksi.php");// sertakan file koneksi database
+$nim = $_GET['nim'];// Dapatkan id dari URL untuk menghapus pengguna itu
+$result = mysqli_query($con, "DELETE FROM mahasiswa WHERE nim=’$nim’");// Hapus baris pengguna dari tabel berdasarkan id yang diberikan
+header("Location:index.php");// Setelah delete redirect ke Home, maka akan muncul daftar user terbaru.
 ?>
