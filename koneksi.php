0bb680f0c4869048f9f15365aa9dd27f3d1a385e
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-10-23 06:44:06 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,11 @@
 <?php
-$host="localhost";
-$username="root";
-$password="";
-$databasename="akademik";
+$host="localhost";//server web
+$username="root";//user name untuk membuka database
+$password="";//password
+$databasename="akademik";//nama database yang digunakan
+//membuat koneksi
 $con=@mysqli_connect($host,$username,$password,$databasename);
+//mengecek koneksi
 if(!$con){
     echo"Error:".mysqli_connnect_error();
     exit();
