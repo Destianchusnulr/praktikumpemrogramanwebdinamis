fcd6be8a0817d2c4b865c9b9d2eb5e68e7bf7635
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-11-06 18:21:29 +0700

    Update hapus_user.php

--- hapus_user.php
+++ hapus_user.php
@@ -1,6 +1,6 @@
 <?php
     include "koneksi.php";
-    $sql="delete from users where id_user= '$_GET[id]'";
+    $sql="delete from users where id_user= '$_GET[id]'";// manghapus data yang sudah tersimpan
     mysqli_query($conn, $sql);
     mysqli_close($conn);
     header('location:tampil_user.php');
