e75b13d1682694bb4c3895a4d2702a13a2579878
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-11-06 18:13:27 +0700

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -3,9 +3,9 @@
 	<h2>Login</h2>
 	<form method=post action=cek_login.php>
 		<table>
-			<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
-			<tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr>
-			<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
+			<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr> // menginputkan nama
+			<tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr> // menginputkan  password
+			<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr> //tombol submit
 		</table>
 	</form>";
 ?>
