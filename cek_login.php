0248c44b363720965d09ba1b7fd78f597587fdcd
Destian Chusnul Ramadhan <62751706+Destianchusnulr@users.noreply.github.com>
2021-11-25 18:59:23 +0700

    Add files via upload

--- cek_login.php
+++ cek_login.php
@@ -1,22 +1,28 @@
 <?php
-    include "koneksi.php";
-    $id_user = $_POST['id_user'];
-    $pass = md5($_POST['paswd']);
-    $sql = "SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'"; //mengecek id_user dan password pada database
-    $login = mysqli_query($conn, $sql);
-    $ketemu = mysqli_num_rows($login);
-    $r = mysqli_fetch_array($login);
-    if ($ketemu > 0){
-        session_start();
-        $_SESSION['iduser'] = $r['id_user'];
-        $_SESSION['passuser'] = $r['password'];
-        echo"USER BERHASIL LOGIN<br>"; //tampilan saat berhasil login
-        echo "id_user = ".$_SESSION['iduser']."<br>"; //session id_user
-        echo "password = ".$_SESSION['passuser']."<br>"; // session password
-        echo "<a href=logout.php><b>LOGOUT</b></a></center>"; // tombol logout
-    }else{
-        echo "<center>Login gagal! username & password tidak benar<br>";// menampilkan jika username dan password ada yang salah
-        echo "<a href=form_login.php><b>ULANGILAGI</b></a></center>";// tombol ulangi memasukan username dan password
-    }
-    mysqli_close($conn);
+	session_start();
+	include "koneksi.php";
+	$id_user = $_POST['id_user'];
+	$pass=md5($_POST['paswd']);
+	$sql="SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'";
+	if ($_POST["captcha_code"] == $_SESSION["captcha_code"]) {
+		$login=mysqli_query($con,$sql);
+		$ketemu=mysqli_num_rows($login);
+		$r= mysqli_fetch_array($login);
+	if ($ketemu > 0){
+		 $_SESSION['iduser'] = $r['id_user'];
+		 $_SESSION['passuser'] = $r['password'];
+		 echo"USER BERHASIL LOGIN<br>";
+		 echo "id user =",$_SESSION['iduser'],"<br>";
+		 echo "password=",$_SESSION['passuser'],"<br>";
+		 echo "<a href=logout.php><b>LOGOUT</b></a></center>";
+	}
+	else{
+		echo "<center>Login gagal! username & password tidak benar<br>";
+		echo "<a href=form_login.php><b>ULANGI LAGI</b></a></center>";
+	}
+	mysqli_close($con);
+	}
+	else {
+		echo "<center>Login gagal! Captcha tidak sesuai<br>";
+		echo "<a href=form_login.php><b>ULANGI LAGI</b></a></center>"; }
 ?>
--- form_login.php
+++ form_login.php
@@ -1,11 +1,12 @@
 <?php
-	echo "
-	<h2>Login</h2>
+	echo "<h2>Login</h2>
 	<form method=post action=cek_login.php>
-		<table>
-			<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr> // menginputkan nama
-			<tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr> // menginputkan  password
-			<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr> //tombol submit
-		</table>
+	<table>
+	<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
+	<tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr>
+	<tr><td>Captcha<br>
+	<img src='captcha.php' /></td><td> : <input name='captcha_code' type='text'></td></tr>
+	<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
+	</table>
 	</form>";
 ?>
